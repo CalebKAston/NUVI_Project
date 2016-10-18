1fcfd549ace529cee1b8ad3bfb71b05d3435ba2d
Caleb Aston <caleb.k.aston@gmail.com>
2016-10-18 13:25:22 -0600

    Started basic react project

---
+++ src/Intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Intro extends Component {
+  render() {
+    return (
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload. Please
+      </p>
+    );
+  }
+}
+
+export default Intro;
